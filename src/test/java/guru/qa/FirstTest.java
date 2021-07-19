3c79bd8e5502d728f18ce88f62d7276011f9c6fc
zimmermanul <habib@e1.ru>
2021-07-19 17:07:21 +0500

    added junit5 support

--- src/test/java/guru/qa/FirstTest.java
+++ src/test/java/guru/qa/FirstTest.java
@@ -8,11 +8,13 @@ public class FirstTest {
     static void BeforeAllah() {
         System.out.println("this is before all method");
 
+
     }
 
     @BeforeEach
     void setUp() {
         System.out.println("this is before each method!");
+
 
     }
 
@@ -24,14 +26,14 @@ public class FirstTest {
 
 
     @Test
-    void firstTest () {
+    void firstTest() {
         System.out.println("This is a first test");
         Assertions.assertTrue(true);
 
     }
 
     @Test
-    void secondTest () {
+    void secondTest() {
         System.out.println("This is a second test");
         Assertions.assertTrue(true);
 

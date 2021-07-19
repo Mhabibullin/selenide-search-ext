b1d4ec39d0d0c3f97bb428e3134a3fdcc44a95d1
zimmermanul <habib@e1.ru>
2021-07-19 15:43:52 +0500

    added junit5 support

---
+++ src/test/java/guru/qa/FirstTest.java
@@ -0,0 +1,40 @@
+package guru.qa;
+
+import org.junit.jupiter.api.*;
+
+public class FirstTest {
+
+    @BeforeAll
+    static void BeforeAllah() {
+        System.out.println("this is before all method");
+
+    }
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("this is before each method");
+
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("this is after each method");
+
+    }
+
+
+    @Test
+    void firstTest () {
+        System.out.println("This is a first test");
+        Assertions.assertTrue(true);
+
+    }
+
+    @Test
+    void secondTest () {
+        System.out.println("This is a second test");
+        Assertions.assertTrue(true);
+
+    }
+
+}
